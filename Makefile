6f20fdadc89a69cc82d7d25a419164bfd038c7b2
BlueCannonBall <mmttiizz100@gmail.com>
2023-10-26 23:54:15 +0000

    Update

---
+++ Makefile
@@ -0,0 +1,32 @@
+CXX = g++
+CXXFLAGS = -Wall -std=c++14 -O3 -pthread
+LDLIBS = -lcrypto -lboost_program_options -lboost_thread
+HEADERS = $(shell find . -name "*.hpp")
+OBJDIR = obj
+OBJS = $(OBJDIR)/main.o $(OBJDIR)/polynet.o $(OBJDIR)/polyweb.o $(OBJDIR)/polyweb_string.o
+PREFIX = /usr/local
+TARGET = declengine
+
+$(TARGET): $(OBJS)
+	$(CXX) $^ $(CXXFLAGS) $(LDLIBS) -o $@
+
+$(OBJDIR)/main.o: main.cpp $(HEADERS)
+	mkdir -p $(OBJDIR)
+	$(CXX) -c $< $(CXXFLAGS) -o $@
+
+$(OBJDIR)/polynet.o: Polyweb/Polynet/polynet.cpp Polyweb/Polynet/polynet.hpp
+	mkdir -p $(OBJDIR)
+	$(CXX) -c $< $(CXXFLAGS) -o $@
+
+$(OBJDIR)/polyweb.o: Polyweb/polyweb.cpp Polyweb/polyweb.hpp Polyweb/Polynet/polynet.hpp Polyweb/string.hpp Polyweb/threadpool.hpp
+	mkdir -p $(OBJDIR)
+	$(CXX) -c $< $(CXXFLAGS) -o $@
+
+$(OBJDIR)/polyweb_string.o: Polyweb/string.cpp Polyweb/string.hpp
+	mkdir -p $(OBJDIR)
+	$(CXX) -c $< $(CXXFLAGS) -o $@
+
+.PHONY: clean
+
+clean:
+	rm -rf $(TARGET) $(OBJDIR)
--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include "Polyweb/polyweb.hpp"
 #include "json.hpp"
 #include <boost/process.hpp>
+#include <iostream>
+#include <limits>
+#include <string>
+#include <cctype>
 
+using nlohmann::json;
 namespace bp = boost::process;
 
 int main() {
     pn::init();
     pn::UniqueSock<pw::Server> server;
 
-    server->route("/word_info", 
+    server->route("/word_info",
         pw::HTTPRoute {
             [](const pw::Connection&, const pw::HTTPRequest& req, void*) {
                 pw::QueryParameters::map_type::const_iterator word_it;
@@ -16,7 +21,86 @@ int main() {
                     return pw::HTTPResponse::make_basic(400);
                 }
 
-                
+                json resp;
+
+                bp::ipstream out;
+                bp::child words("./bin/words", word_it->second, bp::start_dir("whitakers-words"), bp::std_out > out);
+    
+                std::string word, part_of_speech;
+                out >> word >> part_of_speech;
+                resp["word"] = word;
+                resp["part_of_speech"] = part_of_speech;
+
+                int unknown;
+                switch (part_of_speech[0]) {
+                case 'N': {
+                    int declension;
+                    std::string casus, plurality, gender;
+                    out >> declension >> unknown >> casus >> plurality >> gender;
+                    resp["declension"] = declension;
+                    resp["casus"] = casus;
+                    resp["plural"] = plurality[0] == 'P';
+                    resp["gender"] = gender;
+
+                    std::string english_word;
+                    out.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore the rest of the first line
+                    for (std::string line; std::getline(out, line, '\n');) {
+                        pw::string::trim(line);
+                        if (line.back() == ';') {
+                            for (char c : line) {
+                                if (ispunct(c) || isspace(c)) {
+                                    break;
+                                } else {
+                                    english_word.push_back(c);
+                                }
+                            }
+                            break;
+                        }
+                    }
+
+                    // Decline English word
+                    if (plurality[0] == 'P') {
+                        switch (english_word.back()) {
+                        case 'a':
+                        case 'e':
+                        case 'i':
+                        case 'o':
+                        case 'u':
+                            english_word.push_back('e');
+                            break;
+                        }
+                        english_word.push_back('s');
+                    }
+                    resp["english_word"] = english_word;
+
+                    break;
+                }
+
+                case 'V': {
+                    int conjugation;
+                    std::string tense, voice, mood, plurality;
+                    out >> conjugation >> unknown >> tense >> voice >> mood >> unknown >> plurality;
+                    resp["conjugation"] = conjugation;
+                    resp["tense"] = tense;
+                    resp["voice"] = voice;
+                    resp["mood"] = mood;
+                    resp["plural"] = plurality[0] == 'P';
+                    break;
+                }
+
+                case 'A': {
+                    int declension;
+                    std::string casus, plurality, gender;
+                    out >> declension >> unknown >> casus >> plurality >> gender;
+                    resp["declension"] = declension;
+                    resp["casus"] = casus;
+                    resp["plural"] = plurality[0] == 'P';
+                    resp["gender"] = gender;
+                    break;
+                }
+                }
+
+                return pw::HTTPResponse(200, resp.dump(), {{"Content-Type", "application/json"}});
             },
         });
 
@@ -25,6 +109,7 @@ int main() {
         return 1;
     }
 
+    std::cout << "Latin Declengine listening on port 8000" << std::endl;
     if (server->listen() == PN_ERROR) {
         std::cerr << "Error: " << pw::universal_strerror() << std::endl;
         return 1;
