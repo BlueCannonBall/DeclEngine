da7ff970ed9ce97fdefe4e8a2fdc0fb4449e81a1
BlueCannonBall <mmttiizz100@gmail.com>
2023-10-29 14:08:08 -0400

    De-spaghettify

--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
 #include "Polyweb/polyweb.hpp"
-#include "Polyweb/string.hpp"
 #include "json.hpp"
-#include <boost/process.hpp>
-#include <cctype>
+#include "translate.hpp"
 #include <iostream>
-#include <limits>
 #include <string>
 
 using nlohmann::json;
-namespace bp = boost::process;
 
 int main() {
     pn::init();
@@ -22,152 +18,102 @@ int main() {
                     return pw::HTTPResponse::make_basic(400);
                 }
 
-                json resp = json::array();
+                WordInfo word_info = query_whitakers_words(word_it->second);
+                json resp = {
+                    {"split_word", word_info.split_word},
+                    {"variants", json::array()},
+                    {"english_base", word_info.english_base},
+                };
 
-                bp::ipstream out;
-                bp::child words("./bin/words", word_it->second, bp::start_dir("whitakers-words"), bp::std_out > out);
+                for (const auto& variant : word_info.variants) {
+                    json json_variant;
 
-                for (json word_variant; ; word_variant.clear()) {
-                    std::string word, part_of_speech;
-                    out >> word >> part_of_speech;
-                    if (resp.empty()) {
-                        if ((word == "Two" && part_of_speech == "words") || (word == word_it->second && part_of_speech == "========")) { // Check if Latin word wasn't found
-                            return pw::HTTPResponse::make_basic(404);
-                        }
-                    } else if (word != resp[0]["word"]) {
-                        break; // Variants are over
-                    }
-                    word_variant["word"] = word;
-                    word_variant["part_of_speech"] = part_of_speech;
+                    switch (variant->part_of_speech) {
+                    case PART_OF_SPEECH_NOUN: {
+                        json_variant["part_of_speech"] = "noun";
+                        auto noun = (Noun*) variant.get();
 
-                    int unknown;
-                    switch (part_of_speech[0]) {
-                    case 'N': {
-                        int declension;
-                        std::string casus, plurality, gender;
-                        out >> declension >> unknown >> casus >> plurality >> gender;
-                        word_variant["declension"] = declension;
-                        word_variant["casus"] = casus;
-                        word_variant["plural"] = plurality[0] == 'P';
-                        word_variant["gender"] = gender;
-                        break;
-                    }
-
-                    case 'V': {
-                        int conjugation, number;
-                        std::string tense, voice, mood, plurality;
-                        out >> conjugation >> unknown >> tense >> voice >> mood >> number >> plurality;
-                        word_variant["conjugation"] = conjugation;
-                        word_variant["tense"] = tense;
-                        word_variant["voice"] = voice;
-                        word_variant["mood"] = mood;
-                        word_variant["number"] = number;
-                        word_variant["plural"] = plurality[0] == 'P';
-                        break;
-                    }
-
-                    case 'A': {
-                        int declension;
-                        std::string casus, plurality, gender;
-                        out >> declension >> unknown >> casus >> plurality >> gender;
-                        word_variant["declension"] = declension;
-                        word_variant["casus"] = casus;
-                        word_variant["plural"] = plurality[0] == 'P';
-                        word_variant["gender"] = gender;
-                        break;
-                    }
-                    }
-
-                    resp.push_back(word_variant);
-                    out.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Read the rest of the line
-                }
-
-                std::string base_english_word;
-                out.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Read the rest of the line
-                for (char c; out.get(c) && !ispunct(c);) {
-                    base_english_word.push_back(c);
-                }
-                pw::string::trim_right(base_english_word);
-
-                for (auto& word_variant : resp) {
-                    std::string english_word = base_english_word;
-                    switch(word_variant["part_of_speech"][0].get<char>()) {
-                    case 'N': {
-                        if (word_variant["casus"] == "GEN") {
-                            english_word.insert(0, "of ");
-                        } else if (word_variant["casus"] == "DAT") {
-                            english_word.insert(0, "to/for ");
+                        switch (noun->casus) {
+                        case CASUS_NOMINATIVE: json_variant["case"] = "nominative"; break;
+                        case CASUS_GENITIVE: json_variant["case"] = "genitive"; break;
+                        case CASUS_DATIVE: json_variant["case"] = "dative"; break;
+                        case CASUS_ACCUSATIVE: json_variant["case"] = "accusative"; break;
+                        case CASUS_ABLATIVE: json_variant["case"] = "ablative"; break;
+                        case CASUS_VOCATIVE: json_variant["case"] = "vocative"; break;
+                        case CASUS_LOCATIVE: json_variant["case"] = "locative"; break;
                         }
 
-                        if (word_variant["plural"]) {
-                            switch (english_word.back()) {
-                            case 'a':
-                            case 'i':
-                            case 'o':
-                            case 'u':
-                                english_word.push_back('e');
-                                break;
+                        json_variant["plural"] = noun->plural;
 
-                            case 'y':
-                                english_word.pop_back();
-                                english_word += "ie";
-                                break;
-                            }
-                            english_word.push_back('s');
+                        switch (noun->gender) {
+                        case GENDER_MASCULINE: json_variant["gender"] = "masculine"; break;
+                        case GENDER_FEMININE: json_variant["gender"] = "feminine"; break;
+                        case GENDER_NEUTER: json_variant["gender"] = "neuter"; break;
                         }
 
                         break;
                     }
 
-                    case 'V': {
-                        switch (word_variant["number"].get<int>()) {
-                        case 1:
-                            if (word_variant["tense"] == "IMPF") {
-                                english_word.insert(0, word_variant["plural"] ? "were " : "was ");
-                            }
-                            english_word.insert(0, word_variant["plural"] ? "we " : "I ");
-                            break;
+                    case PART_OF_SPEECH_VERB: {
+                        json_variant["part_of_speech"] = "verb";
+                        auto verb = (Verb*) variant.get();
 
-                        case 2:
-                            if (word_variant["tense"] == "IMPF") {
-                                english_word.insert(0, "were ");
-                            }
-                            english_word.insert(0, word_variant["plural"] ? "y'all " : "you ");
-                            break;
+                        json_variant["conjugation"] = verb->conjugation;
 
-                        case 3:
-                            if (word_variant["tense"] == "IMPF") {
-                                english_word.insert(0, "were ");
-                            }
-                            english_word.insert(0, "they ");
-                            break;
+                        switch (verb->tense) {
+                        case TENSE_PRESENT: json_variant["tense"] = "present"; break;
+                        case TENSE_IMPERFECT: json_variant["tense"] = "imperfect"; break;
+                        case TENSE_PERFECT: json_variant["tense"] = "perfect"; break;
+                        case TENSE_PLUPERFECT: json_variant["tense"] = "pluperfect"; break;
+                        case TENSE_FUTURE: json_variant["tense"] = "future"; break;
+                        case TENSE_FUTURE_PERFECT: json_variant["tense"] = "future perfect"; break;
                         }
 
-                        if (word_variant["tense"] == "IMPF") {
-                            if (english_word.back() == 'e') {
-                                english_word.pop_back();
-                            }
-                            english_word += "ing";
-                        } else if (word_variant["tense"] == "PERF") {
-                            if (english_word.back() == 'e') {
-                                english_word.push_back('d');
-                            } else {
-                                english_word += "ed";
-                            }
-                        } else if (word_variant["tense"] == "PLUP") {
-                            english_word.insert(0, "had ");
-                            if (english_word.back() == 'e') {
-                                english_word.push_back('d');
-                            } else {
-                                english_word += "ed";
-                            }
+                        switch (verb->voice) {
+                        case VOICE_ACTIVE: json_variant["voice"] = "active"; break;
+                        case VOICE_PASSIVE: json_variant["voice"] = "passive"; break;
+                        }
+
+                        switch (verb->mood) {
+                        case MOOD_INDICATIVE: json_variant["mood"] = "indicative"; break;
+                        case MOOD_INFINITIVE: json_variant["mood"] = "infinitive"; break;
+                        case MOOD_IMPERATIVE: json_variant["mood"] = "imperative"; break;
+                        case MOOD_SUBJUNCTIVE: json_variant["mood"] = "subjunctive"; break;
+                        }
+
+                        json_variant["plural"] = verb->plural;
+
+                        break;
+                    }
+
+                    case PART_OF_SPEECH_ADJECTIVE: {
+                        json_variant["part_of_speech"] = "adjective";
+                        auto adjective = (Adjective*) variant.get();
+
+                        switch (adjective->casus) {
+                        case CASUS_NOMINATIVE: json_variant["casus"] = "nominative"; break;
+                        case CASUS_GENITIVE: json_variant["casus"] = "genitive"; break;
+                        case CASUS_DATIVE: json_variant["casus"] = "dative"; break;
+                        case CASUS_ACCUSATIVE: json_variant["casus"] = "accusative"; break;
+                        case CASUS_ABLATIVE: json_variant["casus"] = "ablative"; break;
+                        case CASUS_VOCATIVE: json_variant["casus"] = "vocative"; break;
+                        case CASUS_LOCATIVE: json_variant["casus"] = "locative"; break;
+                        }
+
+                        json_variant["plural"] = adjective->plural;
+
+                        switch (adjective->gender) {
+                        case GENDER_MASCULINE: json_variant["gender"] = "masculine"; break;
+                        case GENDER_FEMININE: json_variant["gender"] = "feminine"; break;
+                        case GENDER_NEUTER: json_variant["gender"] = "neuter"; break;
                         }
 
                         break;
                     }
                     }
 
-                    word_variant["english_word"] = english_word;
+                    json_variant["english_equivalent"] = variant->english_equivalent(word_info.english_base);
+                    resp["variants"].push_back(json_variant);
                 }
 
                 return pw::HTTPResponse(200, resp.dump(), {{"Content-Type", "application/json"}});
---
+++ translate.hpp
@@ -0,0 +1,129 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+enum PartOfSpeech {
+    PART_OF_SPEECH_NOUN,
+    PART_OF_SPEECH_VERB,
+    PART_OF_SPEECH_ADJECTIVE,
+};
+
+enum Casus {
+    CASUS_NOMINATIVE,
+    CASUS_GENITIVE,
+    CASUS_DATIVE,
+    CASUS_ACCUSATIVE,
+    CASUS_ABLATIVE,
+    CASUS_VOCATIVE,
+    CASUS_LOCATIVE,
+};
+
+enum Gender {
+    GENDER_MASCULINE,
+    GENDER_FEMININE,
+    GENDER_NEUTER, // Not applicable to proper nouns
+};
+
+enum Tense {
+    TENSE_PRESENT,
+    TENSE_IMPERFECT,
+    TENSE_PERFECT,
+    TENSE_PLUPERFECT,
+    TENSE_FUTURE,
+    TENSE_FUTURE_PERFECT,
+};
+
+enum Voice {
+    VOICE_ACTIVE,
+    VOICE_PASSIVE,
+};
+
+enum Mood {
+    MOOD_INDICATIVE,
+    MOOD_INFINITIVE,
+    MOOD_IMPERATIVE,
+    MOOD_SUBJUNCTIVE,
+};
+
+typedef unsigned short Declension;
+typedef unsigned short Conjugation;
+typedef unsigned short Number;
+
+class WordVariant {
+public:
+    PartOfSpeech part_of_speech;
+
+    WordVariant(PartOfSpeech part_of_speech):
+        part_of_speech(part_of_speech) {}
+
+    virtual std::string english_equivalent(const std::string& english_base) const = 0;
+};
+
+class Noun : public WordVariant {
+public:
+    Declension declension;
+    Casus casus;
+    bool plural;
+    Gender gender;
+
+    Noun(Declension declension, Casus casus, bool plural, Gender gender):
+        WordVariant(PART_OF_SPEECH_NOUN),
+        declension(declension),
+        casus(casus),
+        plural(plural),
+        gender(gender) {}
+
+    std::string english_equivalent(const std::string& english_base) const override;
+};
+
+class Verb : public WordVariant {
+public:
+    Conjugation conjugation;
+    Tense tense;
+    Voice voice;
+    Mood mood;
+    Number number;
+    bool plural;
+
+    Verb(Conjugation conjugation, Tense tense, Voice voice, Mood mood, Number number, bool plural):
+        WordVariant(PART_OF_SPEECH_VERB),
+        conjugation(conjugation),
+        tense(tense),
+        voice(voice),
+        mood(mood),
+        number(number),
+        plural(plural) {}
+
+    std::string english_equivalent(const std::string& english_base) const override;
+};
+
+class Adjective : public WordVariant {
+public:
+    Declension declension;
+    Casus casus;
+    bool plural;
+    Gender gender;
+
+    Adjective(Declension declension, Casus casus, bool plural, Gender gender):
+        WordVariant(PART_OF_SPEECH_ADJECTIVE),
+        declension(declension),
+        casus(casus),
+        plural(plural),
+        gender(gender) {}
+
+    std::string english_equivalent(const std::string& english_base) const override;
+};
+
+struct WordInfo {
+    std::string split_word;
+    std::vector<std::unique_ptr<WordVariant>> variants;
+    std::string english_base;
+
+    bool is_valid() const {
+        return !variants.empty();
+    }
+};
+
+WordInfo query_whitakers_words(const std::string& word);
