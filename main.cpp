c3d0dab4afa87ad3e9650be05db0143e900357d6
BlueCannonBall <mmttiizz100@gmail.com>
2023-11-07 22:24:14 -0500

    Push

--- main.cpp
+++ main.cpp
@@ -100,13 +100,13 @@ int main() {
                                 auto noun = (Noun*) variant.get();
 
                                 switch (noun->casus) {
-                                case CASUS_NOMINATIVE: json_variant["case"] = "nominative"; break;
-                                case CASUS_GENITIVE: json_variant["case"] = "genitive"; break;
-                                case CASUS_DATIVE: json_variant["case"] = "dative"; break;
-                                case CASUS_ACCUSATIVE: json_variant["case"] = "accusative"; break;
-                                case CASUS_ABLATIVE: json_variant["case"] = "ablative"; break;
-                                case CASUS_VOCATIVE: json_variant["case"] = "vocative"; break;
-                                case CASUS_LOCATIVE: json_variant["case"] = "locative"; break;
+                                case CASUS_NOMINATIVE: json_variant["casus"] = "nominative"; break;
+                                case CASUS_GENITIVE: json_variant["casus"] = "genitive"; break;
+                                case CASUS_DATIVE: json_variant["casus"] = "dative"; break;
+                                case CASUS_ACCUSATIVE: json_variant["casus"] = "accusative"; break;
+                                case CASUS_ABLATIVE: json_variant["casus"] = "ablative"; break;
+                                case CASUS_VOCATIVE: json_variant["casus"] = "vocative"; break;
+                                case CASUS_LOCATIVE: json_variant["casus"] = "locative"; break;
                                 }
 
                                 json_variant["plural"] = noun->plural;
@@ -156,13 +156,13 @@ int main() {
                                 auto participle = (Participle*) variant.get();
 
                                 switch (participle->casus) {
-                                case CASUS_NOMINATIVE: json_variant["case"] = "nominative"; break;
-                                case CASUS_GENITIVE: json_variant["case"] = "genitive"; break;
-                                case CASUS_DATIVE: json_variant["case"] = "dative"; break;
-                                case CASUS_ACCUSATIVE: json_variant["case"] = "accusative"; break;
-                                case CASUS_ABLATIVE: json_variant["case"] = "ablative"; break;
-                                case CASUS_VOCATIVE: json_variant["case"] = "vocative"; break;
-                                case CASUS_LOCATIVE: json_variant["case"] = "locative"; break;
+                                case CASUS_NOMINATIVE: json_variant["casus"] = "nominative"; break;
+                                case CASUS_GENITIVE: json_variant["casus"] = "genitive"; break;
+                                case CASUS_DATIVE: json_variant["casus"] = "dative"; break;
+                                case CASUS_ACCUSATIVE: json_variant["casus"] = "accusative"; break;
+                                case CASUS_ABLATIVE: json_variant["casus"] = "ablative"; break;
+                                case CASUS_VOCATIVE: json_variant["casus"] = "vocative"; break;
+                                case CASUS_LOCATIVE: json_variant["casus"] = "locative"; break;
                                 }
 
                                 json_variant["plural"] = participle->plural;
@@ -196,13 +196,13 @@ int main() {
                                 auto supine = (Supine*) variant.get();
 
                                 switch (supine->casus) {
-                                case CASUS_NOMINATIVE: json_variant["case"] = "nominative"; break;
-                                case CASUS_GENITIVE: json_variant["case"] = "genitive"; break;
-                                case CASUS_DATIVE: json_variant["case"] = "dative"; break;
-                                case CASUS_ACCUSATIVE: json_variant["case"] = "accusative"; break;
-                                case CASUS_ABLATIVE: json_variant["case"] = "ablative"; break;
-                                case CASUS_VOCATIVE: json_variant["case"] = "vocative"; break;
-                                case CASUS_LOCATIVE: json_variant["case"] = "locative"; break;
+                                case CASUS_NOMINATIVE: json_variant["casus"] = "nominative"; break;
+                                case CASUS_GENITIVE: json_variant["casus"] = "genitive"; break;
+                                case CASUS_DATIVE: json_variant["casus"] = "dative"; break;
+                                case CASUS_ACCUSATIVE: json_variant["casus"] = "accusative"; break;
+                                case CASUS_ABLATIVE: json_variant["casus"] = "ablative"; break;
+                                case CASUS_VOCATIVE: json_variant["casus"] = "vocative"; break;
+                                case CASUS_LOCATIVE: json_variant["casus"] = "locative"; break;
                                 }
 
                                 json_variant["plural"] = supine->plural;
@@ -272,13 +272,13 @@ int main() {
                                 auto preposition = (Preposition*) variant.get();
 
                                 switch (preposition->casus) {
-                                case CASUS_NOMINATIVE: json_variant["case"] = "nominative"; break;
-                                case CASUS_GENITIVE: json_variant["case"] = "genitive"; break;
-                                case CASUS_DATIVE: json_variant["case"] = "dative"; break;
-                                case CASUS_ACCUSATIVE: json_variant["case"] = "accusative"; break;
-                                case CASUS_ABLATIVE: json_variant["case"] = "ablative"; break;
-                                case CASUS_VOCATIVE: json_variant["case"] = "vocative"; break;
-                                case CASUS_LOCATIVE: json_variant["case"] = "locative"; break;
+                                case CASUS_NOMINATIVE: json_variant["casus"] = "nominative"; break;
+                                case CASUS_GENITIVE: json_variant["casus"] = "genitive"; break;
+                                case CASUS_DATIVE: json_variant["casus"] = "dative"; break;
+                                case CASUS_ACCUSATIVE: json_variant["casus"] = "accusative"; break;
+                                case CASUS_ABLATIVE: json_variant["casus"] = "ablative"; break;
+                                case CASUS_VOCATIVE: json_variant["casus"] = "vocative"; break;
+                                case CASUS_LOCATIVE: json_variant["casus"] = "locative"; break;
                                 }
 
                                 break;
