8097e145f7fa3d4ae64364d432b4a1e92b605082
BlueCannonBall <mmttiizz100@gmail.com>
2023-11-08 16:05:01 +0000

    Implement popping

--- test.py
+++ test.py
@@ -1,32 +1,35 @@
 import requests
 from itertools import groupby
 
-input_text = "nos sumus milites audaces".split(' ')
+input_text = "tu stultum es".split(' ')
 output_text = []
 
 words = []
-offset = 0
 for word in input_text:
     words.append(min(requests.get("http://localhost:8000/word_info", params={"word": word}).json(), key=lambda a: len(a["english_base"])))
 
 # Cull impossible variants
-for i in range(offset, len(words) - 1):
+for i in range(len(words) - 1):
     for preposition in words[i]["variants"]:
         if preposition["part_of_speech"] == "preposition":
             for noun in words[i + 1]["variants"]:
                 if noun["part_of_speech"].endswith("noun") and noun["casus"] == preposition["casus"]:
                     words[i + 1]["variants"] = [noun]
 
+remove = []
+
 # Find subject
-for i in range(offset, len(words)):
+for i in range(len(words)):
     subject_found = False
     for noun in words[i]["variants"]:
         if noun["part_of_speech"].endswith("noun") and noun["casus"] == "nominative":
+            remove.append(words[i])
             adjectives = []
             for j in range(i + 1, len(words)):
                 adjective_found = False
                 for adjective in words[j]["variants"]:
                     if adjective["part_of_speech"] == "adjective":
+                        remove.append(words[j])
                         adjectives.append(adjective)
                         adjective_found = True
                         break
@@ -34,37 +37,44 @@ for i in range(offset, len(words)):
                     break
             for adjective in adjectives:
                 output_text.append(adjective["english_equivalent"])
-                offset += 1
             output_text.append(noun["english_equivalent"])
-            offset += 1
             subject_found = True
             break
     if subject_found:
         break
 
+for word in remove:
+    words.remove(word)
+remove.clear()
+
 # Find verb
-for i in range(offset, len(words)):
+for i in range(len(words)):
     verb_found = False
     for verb in words[i]["variants"]:
         if verb["part_of_speech"] == "verb":
+            remove.append(words[i])
             output_text.append(verb["english_equivalent"])
-            offset += 1
             verb_found = True
             break
     if verb_found:
         break
 
+for word in remove:
+    words.remove(word)
+remove.clear()
 
 # Find object
-for i in range(offset, len(words)):
+for i in range(len(words)):
     object_found = False
     for noun in words[i]["variants"]:
         if noun["part_of_speech"].endswith("noun") and noun["casus"] == "accusative":
+            remove.append(words[i])
             adjectives = []
             for j in range(i + 1, len(words)):
                 adjective_found = False
                 for adjective in words[j]["variants"]:
                     if adjective["part_of_speech"] == "adjective":
+                        remove.append(words[j])
                         adjectives.append(adjective)
                         adjective_found = True
                         break
@@ -72,18 +82,14 @@ for i in range(offset, len(words)):
                     break
             for adjective in adjectives:
                 output_text.append(adjective["english_equivalent"])
-                offset += 1
             output_text.append(noun["english_equivalent"])
-            offset += 1
             object_found = True
     if object_found:
         break
 
-
+for word in remove:
+    words.remove(word)
+remove.clear()
 
 print("LATIN:", ' '.join(input_text))
 print("ENGLISH:", ' '.join([key for key, _group in groupby(' '.join(output_text).split(' '))]))
-
-# for word in words:
-#     print(word["variants"][0]["english_equivalent"]):
-
