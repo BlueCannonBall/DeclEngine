d901349bdd4e640533e5e066fd671d422aa873be
BlueCannonBall <mmttiizz100@gmail.com>
2023-11-14 15:23:18 +0000

    Remove match

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
 import sys
 import requests
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
     print("Error: Too few arguments", file=sys.stderr)
     sys.exit(1)
 
-match sys.argv[1]:
-    case "word":
-        print(requests.get("http://localhost:8000/word_info", params={"word": sys.argv[2]}).content.decode())
-    case "sentence":
-        print(requests.get("http://localhost:8000/sentence_info", params={"sentence": sys.argv[2]}).content.decode())
-    case _:
-        print("Invalid operation", file=sys.stderr)
+if ' ' in sys.argv[1]:
+    print(requests.get("http://localhost:8000/sentence_info", params={"sentence": sys.argv[1]}).content.decode())
+else:
+    print(requests.get("http://localhost:8000/word_info", params={"word": sys.argv[1]}).content.decode())
