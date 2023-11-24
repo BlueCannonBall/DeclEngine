73b5ca67afecfe8eafa4b7da4f7e34828e428dca
BlueCannonBall <mmttiizz100@gmail.com>
2023-11-24 16:42:04 -0500

    Fix datagen

--- datagen.py
+++ datagen.py
@@ -13,21 +13,17 @@ while True:
     line = input_dataset.readline()
     if line:
         split_line = line.strip().split('\t')
-        if i >= 8593:
-            print(split_line)
         latin = split_line[0]
         dirty_english = requests.get(
             "http://localhost:8000/sentence_info", params={"sentence": latin}).content.decode()
         clean_english = split_line[1]
         output_dataset.write(f"{dirty_english}\t{clean_english}\n")
-        if i >= 8593:
-            print(dirty_english, clean_english)
 
         i += 1
-        if i % 100 == 0:
+        if i % 25 == 0:
             print(f"Done with {i} sentence pairs")
     else:
-        if i % 100 != 0:
+        if i % 25 != 0:
             print(f"Done with {i}/{i} sentence pairs")
         break
 
