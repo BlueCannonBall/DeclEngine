d94c30b8434363a7e95865648162365cabc677fc
BlueCannonBall <mmttiizz100@gmail.com>
2023-11-26 14:31:14 -0500

    Make private

--- dictionary.hpp
+++ dictionary.hpp
@@ -31,13 +31,14 @@ struct WordVariant {
 };
 
 class WhitakersWords {
+private:
+    locale_t us_locale = newlocale(LC_CTYPE_MASK, "en_US.utf8", nullptr);
+    iconv_t cd = iconv_open("ASCII//TRANSLIT", "UTF-8");
+
 public:
     boost::process::ipstream out;
     boost::process::opstream in;
     boost::process::child child;
-
-    locale_t us_locale = newlocale(LC_CTYPE_MASK, "en_US.utf8", nullptr);
-    iconv_t cd = iconv_open("ASCII//TRANSLIT", "UTF-8");
 
     WhitakersWords(const std::string& binary = "bin/words", const std::string& start_dir = "whitakers-words"):
         child(binary, boost::process::start_dir(start_dir), boost::process::std_out > out, boost::process::std_in < in) {}
